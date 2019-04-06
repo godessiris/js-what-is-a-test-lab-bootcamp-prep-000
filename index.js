7e2153ee7ed1dd8e9fe56358d4173824fc6a0361
Iris Quintero <godessiris@gmail.com>
2019-04-06 03:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // write your code below!
 var name = "Susan"
 var height = 31
-var message = ` is ${height} inches tall`
-var message = ` ${name} is my name `
+var message = ` ${name} is ${height} inches tall`
+
+
 
 
 module.exports = {name, height, message}
